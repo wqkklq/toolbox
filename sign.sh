228c9bf472fa37cda2c303db7746d09a41fc6219
Shangzhen Yang <shangzheny@outlook.com>
2017-12-27 14:39:49 -0500

    4.2

--- sign.sh
+++ sign.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
 set -ex
-
 APP="RTH Toolbox"
-
 electron-packager . \
   "$APP" \
   --asar \
@@ -16,10 +13,9 @@ electron-packager . \
   --arch=x64 \
   --icon=./electron/electron.icns \
   --prune=true \
-  --out=out \
+  --out=electron/out \
   --extend-info=./electron/info.plist \
   --electron-version=1.7.9
-
 APP_PATH="./electron/out/$APP-mas-x64/$APP.app"
 RESULT_PATH="./electron/out/$APP.pkg"
 APP_KEY="3rd Party Mac Developer Application: Shangzhen Yang (7564J9XM8X)"
@@ -27,7 +23,6 @@ INSTALLER_KEY="3rd Party Mac Developer Installer: Shangzhen Yang (7564J9XM8X)"
 FRAMEWORKS_PATH="$APP_PATH/Contents/Frameworks"
 CHILD_PLIST="./electron/child.plist"
 PARENT_PLIST="./electron/parent.plist"
-
 codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/Electron Framework.framework/Versions/A/Electron Framework"
 codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/Electron Framework.framework/Versions/A/Libraries/libffmpeg.dylib"
 codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/Electron Framework.framework/Versions/A/Libraries/libnode.dylib"
@@ -39,7 +34,5 @@ codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/$APP H
 codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/$APP Helper NP.app/Contents/MacOS/$APP Helper NP"
 codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$FRAMEWORKS_PATH/$APP Helper NP.app/"
 codesign -s "$APP_KEY" -f --entitlements "$CHILD_PLIST" "$APP_PATH/Contents/MacOS/$APP"
-
 codesign -s "$APP_KEY" -f --entitlements "$PARENT_PLIST" "$APP_PATH"
-
 productbuild --component "$APP_PATH" /Applications --sign "$INSTALLER_KEY" "$RESULT_PATH"
