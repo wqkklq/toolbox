851915854b6c29d5abda3f8c4ba62b4368ccea04
Shangzhen Yang <20525492+shangzhenyang@users.noreply.github.com>
2018-12-02 11:33:57 -0500

    16.0

--- js/general.js
+++ js/general.js
@@ -30,7 +30,7 @@ isWeChat=/MicroMessenger\//i.test(navigator.userAgent),
 isWindows=/Windows/i.test(navigator.userAgent),
 langOpt,
 language=localStorage.getItem("Language"),
-lastUpdated=new Date("2018/12/2").toLocaleDateString(),
+lastUpdated=new Date("2018/12/3").toLocaleDateString(),
 login={
 	"email":localStorage.getItem("Email"),
 	"password":localStorage.getItem("Password"),
